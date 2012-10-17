3bec7937db22e74ed9cea38750ea9083f9deccad
echteinfachtv <info@echteinfach.tv>
2012-10-17 19:39:42 +0200

    first commit
    Signed-off-by: echteinfachtv <info@echteinfach.tv>

---
+++ qa-new-users-lang.php
@@ -0,0 +1,41 @@
+<?php
+	
+/*
+	Question2Answer (c) Gideon Greenspan
+
+	http://www.question2answer.org/
+
+	
+	File: qa-plugin/example-page/qa-example-lang-default.php
+	Version: See define()s at top of qa-include/qa-base.php
+	Description: US English language phrases for example plugin
+
+
+	This program is free software; you can redistribute it and/or
+	modify it under the terms of the GNU General Public License
+	as published by the Free Software Foundation; either version 2
+	of the License, or (at your option) any later version.
+	
+	This program is distributed in the hope that it will be useful,
+	but WITHOUT ANY WARRANTY; without even the implied warranty of
+	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+	GNU General Public License for more details.
+
+	More about this license: http://www.question2answer.org/license.php
+*/
+
+	/* this file is UTF-8 encoded, you can use your language characters without worries */
+	
+	return array(
+		'page_title' => 'Neueste Mitglieder der letzten Tage', // newest users of last days
+		'page_days' => 'Tage', // days
+		'user_since' => 'Mitglied seit', // user since
+		'user_name' => 'Mitgliedsname', // username
+		// subnavigation on page users
+		'subnav_title' => 'Neueste Mitglieder', // newest users
+	);
+	
+
+/*
+	Omit PHP closing tag to help avoid accidental output
+*/
---
+++ qa-new-users-page.php
@@ -0,0 +1,101 @@
+<?php
+
+	class qa_new_users_page {
+		
+		var $directory;
+		var $urltoroot;
+		
+		function load_module($directory, $urltoroot)
+		{
+			$this->directory=$directory;
+			$this->urltoroot=$urltoroot;
+		}
+		
+		// for display in admin interface under admin/pages
+		function suggest_requests() 
+		{	
+			return array(
+				array(
+					'title' => 'Newest Users Page', // title of page
+					'request' => 'newusers', // request name
+					'nav' => 'M', // 'M'=main, 'F'=footer, 'B'=before main, 'O'=opposite main, null=none
+				),
+			);
+		}
+		
+		// for url query
+		function match_request($request)
+		{
+			if ($request=='newusers') {
+				return true;
+			}
+
+			return false;
+		}
+
+		function process_request($request)
+		{
+		
+			/* SETTINGS */
+			$lastdays = 14; 			// show new users from last x days
+			$maxusers = 30;				// max new users to display
+			$creditDeveloper = true;	// leave true if you like this plugin, it sets one hidden link to my q2a-forum from the new-user-page only
+			
+			/* start */
+			$qa_content=qa_content_prepare();
+
+			// add sub navigation
+			// $qa_content['navigation']['sub']=qa_users_sub_navigation();
+			$qa_content['title'] = qa_lang_html('qa_new_users_lang/page_title'); // page title
+
+			// counter for custom html output
+			$c = 2;
+			
+			// query users
+			$queryRecentUsers = qa_db_query_sub("SELECT userid,created,handle,avatarblobid,avatarwidth,avatarheight,email,flags
+											FROM `^users`
+											WHERE created > NOW() - INTERVAL ".$lastdays." DAY
+											ORDER BY created DESC
+											LIMIT 0,".$maxusers.";"); 
+
+			// initiate output string
+			$newestusers = "<table> <thead><tr><th class='column1'>".qa_lang_html('qa_new_users_lang/user_since')."</th> <th class='column2'>".qa_lang_html('qa_new_users_lang/user_name')."</th> </tr></thead>";
+			$d = 0;
+			while ( ($userrow = qa_db_read_one_assoc($queryRecentUsers,true)) !== null ) {
+				// do not list blocked users
+				if (! (QA_USER_FLAGS_USER_BLOCKED & $userrow['flags'])) {
+					$avatar = "-";
+					if(!empty($userrow['avatarblobid'])) {
+						$avatar = "<img src='?qa=image&qa_blobid=". $userrow['avatarblobid'] . "&qa_size=30' />";			
+					}
+					// substr removes seconds
+					$newestusers .= "<tr><td>".substr($userrow['created'],0,16)."</td> <td>". qa_get_user_avatar_html($userrow['flags'], $userrow['email'], $userrow['handle'], $userrow['avatarblobid'], $userrow['avatarwidth'], $userrow['avatarheight'], qa_opt('avatar_users_size'), false) . " " . qa_get_one_user_html($userrow['handle'], false) . "</tr>";
+				}
+			}
+			$newestusers .= "</table>";
+
+			
+			/* output into theme */
+			$qa_content['custom'.++$c]='<div class="newusers" style="border-radius:0; padding:0; margin-top:-2px;">';
+			
+			$qa_content['custom'.++$c]= $newestusers;
+			
+			$qa_content['custom'.++$c]='</div>';
+			
+			// make newest users list bigger on page
+			$qa_content['custom'.++$c] = '<style type="text/css">table thead tr th,table tfoot tr th{background-color:#cfc;border:1px solid #CCC;padding:4px} table{background-color:#EEE;margin:30px 0 15px;text-align:left;border-collapse:collapse} td{border:1px solid #CCC;padding:1px 10px;line-height:25px}tr:hover{background:#ffc} .column1, .column2 {text-align:center; } td img{border:1px solid #DDD !important; margin-right:5px;} </style>';
+			
+			// as I said, this is one chance to say thank you
+			if($creditDeveloper) {
+				$qa_content['custom'.++$c] = "<a style='display:none' href='http://www.gute-mathe-fragen.de/'>Gute Mathe-Fragen! * Bestes Mathe-Forum</a>";
+			}
+			
+			return $qa_content;
+		}
+		
+	};
+	
+
+/*
+	Omit PHP closing tag to help avoid accidental output
+*/
